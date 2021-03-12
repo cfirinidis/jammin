825569e3826795f107984440c2589cfb732fad27
cfirinidis <cfirinidis@gmail.com>
2021-03-12 15:30:06 -0500

    Test commit.

---
+++ src/Components/App/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+import SearchBar from '../Searchbar/Searchbar'
+import SearchResults from '../SearchResults/SearchResults'
+import Playlist from '../Playlist/Playlist'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
+
+          < Add a SearchBar component >
+
+          <div class="App-playlist">
+          
+          <Add a SearchResults component >
+
+          < Add a Playlist component >
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import Reaxr from  'react';
+
+
+class SearchBar extends React.Component {
+	render(){
+		return (
+			<div class="SearchBar">
+  				<input placeholder="Enter A Song, Album, or Artist" />
+  				<button class="SearchButton">SEARCH</button>
+			</div>			
+		)
+	}
+
+}
