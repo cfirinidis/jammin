69b4ea73545b58d400a702a834322312774afad7
cfirinidis <cfirinidis@gmail.com>
2021-03-17 18:36:40 -0400

    Finalize Jammin app.

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,14 +1,36 @@
-import Reaxr from  'react';
+import React from  'react';
 
+import './SearchBar.css';
 
 class SearchBar extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			term: ''
+		}
+		this.search = this.search.bind(this);
+		this.handleTermChange = this.handleTermChange.bind(this);
+	}
+
+	search() {
+		this.props.onSearch(this.state.term);
+	}
+
+	handleTermChange(event) {
+		this.setState({term: event.target.value});
+	}
+
+
 	render(){
 		return (
-			<div class="SearchBar">
-  				<input placeholder="Enter A Song, Album, or Artist" />
-  				<button class="SearchButton">SEARCH</button>
+			<div className="SearchBar">
+  				<input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+  				<button className="SearchButton" onClick={this.search}   >SEARCH</button>
 			</div>			
-		)
+		);
 	}
 
 }
+
+
+export default SearchBar;
